f5b33b7f1f4c29b2e29023d7de0c5fda3b301156
victorzywei <victor.zhiyu.wei@gmail.com>
2026-02-25 00:31:03 +0800

    fix: use standard response clone API

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -17,15 +17,9 @@ export async function onRequest(context) {
     }
 
     const response = await context.next();
+    const cloned = new Response(response.body, response);
+    cloned.headers.set('Access-Control-Allow-Origin', '*');
+    cloned.headers.set('Access-Control-Allow-Headers', 'Content-Type, X-Admin-Key, X-Node-Token');
 
-    // We must clone the response to safely modify headers in Cloudflare Pages.
-    const newHeaders = new Headers(response.headers);
-    newHeaders.set('Access-Control-Allow-Origin', '*');
-    newHeaders.set('Access-Control-Allow-Headers', 'Content-Type, X-Admin-Key, X-Node-Token');
-
-    return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders,
-    });
+    return cloned;
 }
